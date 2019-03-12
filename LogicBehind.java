0b62c0f3a85d301b64cb6458c461206fb16392d7
Raman Nimje <32708837+ramannimje@users.noreply.github.com>
2019-03-12 09:41:54 +0530

    Add files via upload

--- LogicBehind.java
+++ LogicBehind.java
@@ -12,7 +12,7 @@ import java.util.logging.Logger;
 public class LogicBehind {
 	private static Logger logger = Logger.getLogger(MainApplication.class.getName());
 	private NavigableMap<Integer, String> hm = new TreeMap<Integer, String>();
-	
+
 	public NavigableMap<Integer, String> getResponse() {
 		System.out.println("Ingesting the value inside the HashMap");
 		hm.put(1, "Awe");
@@ -73,59 +73,62 @@ public class LogicBehind {
 
 	public NavigableMap<Integer, String> reverseHashMap(){
 		try {
-		System.out.println("The Orignal Hashmap looks like: "+ hm.values());
-		System.out.println("\n");
-		System.out.println("After Reversing the Hashmap it looks like: ");
-		for(int i=0; i<=hm.size()-1; i++) {
-			System.out.println(hm.get(hm.size()-i));
-		}
-		System.out.println("Creating the derieved Hashmap: ");
-		LinkedHashMap<Integer, String> reversedhm = new LinkedHashMap<Integer, String>();
+			System.out.println("The Orignal Hashmap looks like: "+ hm.values());
+			System.out.println("\n");
+			System.out.println("After Reversing the Hashmap it looks like: ");
+			for(int i=0; i<=hm.size()-1; i++) {
+				System.out.println(hm.get(hm.size()-i));
+			}
+			System.out.println("Creating the derieved Hashmap: ");
+			LinkedHashMap<Integer, String> reversedhm = new LinkedHashMap<Integer, String>();
 
-		NavigableSet<Integer> hmSet = hm.navigableKeySet();
-		Iterator<Integer> itr = hmSet.descendingIterator();
-		Integer k;
-		while(itr.hasNext()) {
-			k = itr.next();
-			reversedhm.put(k, hm.get(k));
-		}
-		System.out.println("\nReversed HashMap: "+reversedhm.values());
-		logger.info("This is achived by converting the normal Hashmap to the Navigable HashMap and "
-				+ "New Reversed hashmap is the LinkedHashMap");
+			NavigableSet<Integer> hmSet = hm.navigableKeySet();
+			Iterator<Integer> itr = hmSet.descendingIterator();
+			Integer k;
+			while(itr.hasNext()) {
+				k = itr.next();
+				reversedhm.put(k, hm.get(k));
+			}
+			System.out.println("\nReversed HashMap: "+reversedhm.values());
+			logger.info("This is achived by converting the normal Hashmap to the Navigable HashMap and "
+					+ "New Reversed hashmap is the LinkedHashMap");
 		}
 		catch (Exception e) {
 			logger.warning("Error while reversing the HashMap..");
 		}
 		return hm;
 	}
-	
-	public String serialKeyGenerator() {
+
+	public String serialKeysGenerator() {
 		String serialKey = null;
 		Character dashChar = new Character('-');
 		String sb = null;
 		if(serialKey == null) {
-		 serialKey = getSaltString();
-		 System.out.println(serialKey);
-		 int serialKeyLength = serialKey.length();
-		 System.out.println(serialKeyLength);
-		 
-		 sb = serialKey;
+			for(int l=1; l<=hm.size(); l++) {
+				serialKey = hm.get(l);
+				System.out.println("The Generated Random value is:" +serialKey);
+				
+				int serialKeyLength = serialKey.length();
+				System.out.println("The length of Random value is: " +serialKeyLength);
+				
+				logger.info("Your Serial key of Product is: ");
 
-		 String temp = null;
-		 int j=0;
-			 for(int i=1; i<serialKey.length(); i++) {
-				 if(i%3 == 0)
-				 {
-					 //System.out.println(sb.substring(j, i));
-					 //System.out.println(sb.substring(i, i+3) + dashChar);
-					 temp = sb.substring(j, i) + dashChar;
-					 temp = temp + sb.substring(i, i+3) + dashChar;
-					 System.out.println(temp);
-					 j = j+3;
-				 }
-				 //System.out.println(temp);
-				 
-			 }
+				sb = serialKey;
+				String temp = null;
+				int j=0;
+				for(int i=1; i<=serialKey.length(); i++) {
+					if(i%3 == 0)
+					{
+						temp = sb.substring(j, i) + dashChar;
+						if(i==18) {
+							temp = temp.substring(0, 3);
+						}
+						System.out.print(temp);
+						j = j+3;
+					}
+				}
+				System.out.println("\n");
+			}
 		}
 		return serialKey;
 	}
